1d502bd94234652a2f8b87adbd31e00500d8866f
Rouizi <cinorouizi@hotmail.fr>
2020-05-11 03:01:36 +0200

    add order-summary and manage-order-summary to urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ItemDetailView, add_to_cart
+from .views import ItemDetailView, OrderSummary, manage_order_item
 
 
 urlpatterns = [
     path('product/<pk>/<slug:slug>/', ItemDetailView.as_view(), name='product'),
+    path('order-summary/', OrderSummary.as_view(), name='order-summary'),
+    path('manage-order-item/<pk>/', manage_order_item, name='manage-order-item')
 ]

4994f5efc7d8e05f11901ee2c23b44a79447af1d
Rouizi <cinorouizi@hotmail.fr>
2020-06-05 21:38:35 +0200

    Adding the StripePayment model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from core.models import Order
+
+
+class StripePayment(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        related_name='customer',
+        blank=True,
+        null=True
+    )
+    order = models.OneToOneField(
+        Order, on_delete=models.SET_NULL, blank=True, null=True)
+    payment_intent_id = models.CharField(max_length=50, blank=True, null=True)

b7e6255e52fd03757974cc57b842d4e02f815a5d
rouizi <cinorouizi@gmail.com>
2021-03-18 14:54:02 +0100

    configuration for production

--- ecommerce/settings/development.py
+++ ecommerce/settings/development.py
@@ -11,3 +11,14 @@ STRIPE_PUBLISHABLE_KEY_TEST = config('STRIPE_PUBLISHABLE_KEY_TEST')
 STRIPE_SECRET_KEY_TEST = config('STRIPE_SECRET_KEY_TEST')
 # Stripe endpoint's secret for webhooks
 ENDPOINT_SECRET = config('ENDPOINT_SECRET')
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
